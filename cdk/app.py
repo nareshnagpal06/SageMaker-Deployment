9c490028a0ad5c9b357465652e3d6cf86ce4902c
nareshnagpal06 <33876096+nareshnagpal06@users.noreply.github.com>
2025-07-26 22:00:36 +0100

    Delete cdk directory

--- cdk/app.py
+++
@@ -1,7 +0,0 @@
-#!/usr/bin/env python3
-import aws_cdk as cdk
-from model_deployment_stack import ModelDeploymentStack
-
-app = cdk.App()
-ModelDeploymentStack(app, "ModelDeploymentStack")
-app.synth()
